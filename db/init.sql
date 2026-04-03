f98d4c30b1238256c162830e767130698a1c8919
Kr1sw0rk <kriswork@pm.me>
2026-04-03 15:26:42 +0200

    chore: remove Tina CMS

--- db/init.sql
+++
@@ -1,7 +0,0 @@
--- Tina CMS D1 database initialisation
--- Run once: wrangler d1 execute digid-co-tina --file=./db/init.sql
-
-CREATE TABLE IF NOT EXISTS tina_store (
-  key   TEXT PRIMARY KEY NOT NULL,
-  value TEXT NOT NULL
-);
