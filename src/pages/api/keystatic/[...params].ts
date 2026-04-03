dde2cbd5f593439d80de975f7bfa83e47839e8d1
Kr1sw0rk <kriswork@pm.me>
2026-04-03 16:25:29 +0200

    fix: remove manual Keystatic API route (injected by integration)

--- src/pages/api/keystatic/[...params].ts
+++
@@ -1,7 +0,0 @@
-export const prerender = false;
-import { makeRouteHandler } from '@keystatic/astro/api';
-import keystaticConfig from '../../../../keystatic.config';
-
-export const { GET, POST } = makeRouteHandler({
-  config: keystaticConfig,
-});
